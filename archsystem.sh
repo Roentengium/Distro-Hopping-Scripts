cf8f6711f3f20fcfca93f9cc7cb429c109f953ed
Roentengium <cheemsgayming@gmail.com>
2025-07-29 12:41:52 -0400

    Add files via upload

---
+++ archsystem.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#run with sudo you unintelligent bastard
+
+pacman -S konsole steam git flatpak
+
+#dont come after me for installing another terminal, i like konsole
+
+flatpak install flathub org.vinegarhq.Sober 
+flatpak install flathub org.gnome.Quadrapassel
+
+#i need some dumb fun games ok hop off
+
+wget https://www.adirondackalmanack.com/wp-content/uploads/2014/07/Blue-Mountain-Lake-from-Castle-Rock-9.jpg
+
+#peak background + i went here all the time as a kid
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+#the AUR is litterally a necessity
+
